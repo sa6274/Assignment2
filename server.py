60dd1f60c5a742daea5bdf05429f9c29600f2cb4
Chandu <chandu@chandus-mbp.myfiosgateway.com>
2020-10-03 17:49:42 -0400

    Second Commit

--- server.py
+++ server.py
@@ -1,51 +1,48 @@
-# import socket module
+#import socket module
 from socket import *
-import sys  # In order to terminate the program
-
-HEADER = 64
-PORT = 13331
-SERVER = "192.168.1.179"
-ADDR = (SERVER,PORT)
+import sys # In order to terminate the program
 
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
     # Prepare a sever socket
-    serverSocket.bind(ADDR)
+    serverSocket.bind(("", port))
     serverSocket.listen(1)
 
     while True:
-        # Establish the connection
+        #Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr =  serverSocket.accept()
+        connectionSocket, addr = serverSocket.accept()
+
         try:
             message = connectionSocket.recv(1024)
             filename = message.split()[1]
+
             f = open(filename[1:])
             outputdata = f.read()
 
-            # Send one HTTP header line into socket
-            connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n')
+            #Send one HTTP header line into socket
+            connectionSocket.send(bytes("HTTP/1.1 200 OK \r\n\r\n", "UTF-8"))
 
-            # Send the content of the requested file to the client
+
+            #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
-                connectionSocket.send("\r\n".encode())
+            connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
-            # Send response message for file not found (404)
-            connectionSocket.send('404 Not Found')
+            #Send response message for file not found (404)
 
-            # Close client socket
+            connectionSocket.send(bytes("HTTP/1.1 400 Not Found \r\n\r\n", "UTF-8"))
+            connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", "UTF-8"))
+
+
+            #Close client socket
             connectionSocket.close()
+
 
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
 
-
 if __name__ == "__main__":
     webServer(13331)
-
-
-
-
